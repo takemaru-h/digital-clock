a3c53d8eafd9ba42bd47e551b1245e23e6722ab8
takemaru <takemaru.h@icloud.com>
2025-10-24 13:45:23 +0900

    Delete chikumaru directory

--- chikumaru/script_popup.js
+++
@@ -1,9 +0,0 @@
-setInterval(() => {
-  const x = window.screenX;
-  const y = window.screenY;
-
-  // 親ウィンドウが存在するなら、位置情報を送る
-  if (window.opener) {
-    window.opener.postMessage({ x, y }, "*");
-  }
-}, 100);
